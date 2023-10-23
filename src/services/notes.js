348c4f262a0121b1943b85e8261467c6771e32fa
spmanolo <manolosan2001@gmail.com>
2023-10-23 20:47:59 +0200

    modificado modelo de notas

--- src/services/notes.js
+++ src/services/notes.js
@@ -2,25 +2,40 @@ import axios from 'axios'
 
 const URL = 'http://localhost:3001/api/notes/'
 
+let token = null
+
+function setToken(newToken) {
+  token = `bearer ${newToken}`
+}
+
 function getAll() {
   return axios.get(URL)
     .then(response => response.data)
 }
 
 function create(newNote) {
+  const config = {
+    headers: { Authorization: token }
+  }
+
   return axios
-    .post(URL, newNote)
+    .post(URL, newNote, config)
     .then(response => response.data)
 }
 
 function update(changedNote) {
+  const config = {
+    headers: { Authorization: token }
+  }
+
   return axios
-    .put(URL + changedNote.id, changedNote)
+    .put(URL + changedNote.id, changedNote, config)
     .then(response => response.data)
 }
 
 export default {
   getAll,
   create,
-  update
+  update,
+  setToken
 }
